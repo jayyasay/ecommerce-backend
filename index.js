72add471e9fb5bc2bdf234a57f81c8cfb7614d35
Jay Yasay <paolo_yasay@yahoo.com>
2023-08-03 12:14:20 +0400

    Pushing my API 6

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-// index.js
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = 4000;
 
-const app = express()
-const PORT = 4000
+require('dotenv').config();
 
-require("dotenv").config();
-
-const mongoose = require("mongoose");
-
-const ImageModel = require("./image.model");
+const ImageModel = require('./image.model');
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+  process.exit(1); // Exit the application if MongoDB connection fails
 });
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+  console.log(`API listening on PORT ${PORT}`);
+});
 
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+  res.send('Hey this is my API running 🥳');
+});
 
 app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+  res.send('This is my about route.....');
+});
 
-app.get("/api/db/products", (req, res) => {
-    ImageModel.find().then((details) => {
-      res.json(details);
-    });
+app.get('/api/db/products', (req, res) => {
+  ImageModel.find().then((details) => {
+    res.json(details);
+  }).catch((err) => {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
   });
-
-// Export the Express API
-module.exports = app
+});
